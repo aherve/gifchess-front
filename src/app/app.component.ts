7dfd987963b22a7f9ad2c9c8b5de4fcbd275a00a
aherve <mail@aurelien-herve.com>
2019-06-15 11:51:46 +0200

    initial commit: clone app from existing gifchess project

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ah-root',
+  template: `<ah-layout></ah-layout>`,
+  styles: []
+})
+export class AppComponent {
+  constructor() {}
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  analyticsToken: 'UA-28822787-3',
+};
